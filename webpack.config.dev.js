2de1dadec4cfe18a3399aadb90a10493699ddeba
alonzathompson <thompson0005@gmail.com>
2016-12-05 16:26:17 -0500

    Added hot Reloading

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,17 +1,30 @@
 import path from 'path';
+import webpack from 'webpack';
 
 export default {
 	devtools: 'eval-source-map',
-	entry: path.join(__dirname, '/client/index.js'),
+	entry: [
+		'webpack-hot-middleware/client',
+
+		path.join(__dirname, '/client/index.js'),
+	],
 	output: {
-		path: '/'
+		path: '/',
+		publicPath: '/'
 	},
+	plugins: [
+		//makes cleaner error messages
+		new webpack.NoErrorsPlugin(),
+		//ensures consistant hashes
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	],
 	module: {
 		loaders: [
 			{
 				test: /\.js$/,
 				include: path.join(__dirname, 'client'),
-				loaders: [ 'babel']
+				loaders: [ 'react-hot', 'babel']
 			}
 		]
 	},
