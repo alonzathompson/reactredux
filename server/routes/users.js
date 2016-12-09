941cc5526b480f61271a05e71c4c642352692542
alonzathompson <thompson0005@gmail.com>
2016-12-09 10:57:07 -0500

    Validate user and email on server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,17 +1,41 @@
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 import bcrypt from 'bcrypt';
+import isEmpty from 'lodash/isEmpty';
 
 import User from '../models/user';
 
 let router = express.Router();
 
+function validateInput(data, otherValidations) {
+	let { errors } = otherValidations(data);
+
+	return User.query({
+		where: { email: data.email },
+		orWhere: {username: data.username}
+	}).fetch().then(user => {
+		if(user) {
+			if(user.get('username') === data.username){
+				errors.username = "This username already exist";
+			}
+			if(user.get('email') === data.email){
+				errors.email = "This email already exist";
+			}
+		}
+		return {
+			errors,
+			isValid: isEmpty(errors)
+		};
+	})
+		
+}
+
 
 router.post('/', (req, res) => {
 	console.log(req.body);
-	const { errors, isValid } = validateInput(req.body);
-
-	if(isValid) {
+	validateInput(req.body, commonValidations)
+	.then(({ errors, isValid }) => {
+		if(isValid) {
 		const {username, password, timezone, email } =req.body;
 		const password_digest = bcrypt.hashSync(password, 10);
 
@@ -20,9 +44,11 @@ router.post('/', (req, res) => {
 		}, { hasTimestamps: true }).save()
 			.then(user => res.json({ success: true }))
 			.catch(err => res.status(500).json({ error: error }));
-	} else {
-		res.status(400).json(errors);
-	}
+		} else {
+			res.status(400).json(errors);
+		}
+	});
+
 });
 
 export default router;
